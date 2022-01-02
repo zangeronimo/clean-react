d3a8cabf786ad178333a725b691f73b0e719c0bc
Luciano Zangeronimo <zangeronimo@gmail.com>
2022-01-02 09:50:05 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentations/pages/login/index.tsx
+++ src/presentations/pages/login/index.tsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Footer,
   FormStatus,
   Input,
   LoginHeader
 } from '@/presentations/components'
+import Context from '@/presentations/contexts/form/form-context'
+
 import Styles from './styles.scss'
 
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <form className={Styles.form}>
-        <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className={Styles.submit} type="submit">Entrar</button>
-        <span className={Styles.link}>Criar conta</span>
-        <FormStatus />
-      </form>
+      <Context.Provider value={state}>
+        <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <button className={Styles.submit} type="submit">Entrar</button>
+          <span className={Styles.link}>Criar conta</span>
+          <FormStatus />
+        </form>
+      </Context.Provider>
       <Footer />
     </div>
   )
